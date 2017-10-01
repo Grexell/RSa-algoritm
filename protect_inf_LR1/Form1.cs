a297b6b20593715cb447c2fa4aeb9fc621928d1e
bbbbbbbbb <demesh.dima.dd@gmail.com>
2017-10-01 18:57:36 +0300

    init commit

---
+++ protect_inf_LR1/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Numerics;
+
+namespace protect_inf_LR1
+{
+    public partial class Form1 : Form
+    {
+        char[] characters = new char[] { '#', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+                                                        'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 
+                                                        'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+                                                        'Э', 'Ю', 'Я', ' ', '1', '2', '3', '4', '5', '6', '7',
+                                                        '8', '9', '0' };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //зашифровать
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            if ((textBox_p.Text.Length > 0) && (textBox_q.Text.Length > 0))
+            {
+                
+                long p = genSimpleLong(1024);
+                long q = genSimpleLong(Convert.ToInt32(p));
+
+                textBox_p.Text = p + "";
+                textBox_q.Text = q + "";
+
+
+                if (IsTheNumberSimple(p) && IsTheNumberSimple(q))
+                {
+                    string s = richTextBox1.Text;
+
+                    s = s.ToUpper();
+
+                    long n = p * q;
+                    long m = (p - 1) * (q - 1);
+                    long d = Calculate_d(m);
+                    long e_ = Calculate_e(d, m);
+
+                    List<string> result = RSA_Encode(s, e_, n);
+
+
+                    richTextBox2.Lines = result.ToArray();
+                    
+                    textBox_d.Text = d.ToString();
+                    textBox_n.Text = n.ToString();
+                    
+                }
+                else
+                    MessageBox.Show("p или q - не простые числа!");
+            }
+            else
+                MessageBox.Show("Введите p и q!");
+        }
+
+        //расшифровать
+        private void buttonDecipher_Click(object sender, EventArgs e)
+        {
+            if ((textBox_d.Text.Length > 0) && (textBox_n.Text.Length > 0))
+            {
+                long d = Convert.ToInt64(textBox_d.Text);
+                long n = Convert.ToInt64(textBox_n.Text);
+
+                List<string> input = new List<string>();
+
+                foreach (string str in richTextBox2.Lines) {
+
+                    input.Add(str);
+
+                }
+
+                string result = RSA_Decode(input, d, n);
+
+                richTextBox1.Text = result;
+                
+            }
+            else
+                MessageBox.Show("Введите секретный ключ!");
+        }
+
+        private long genSimpleLong(int n) {
+
+            long result = 1;
+
+            if (n > 1) {
+
+                Random r = new Random(n);
+
+                result = r.Next(1, n);
+
+                while (!IsTheNumberSimple(result)) {
+
+                    result = r.Next(1, n);
+
+                }
+
+
+            }
+
+            return result;
+
+        }
+
+        //проверка: простое ли число?
+        private bool IsTheNumberSimple(long n)
+        {
+            if (n < 2)
+                return false;
+
+            if (n == 2)
+                return true;
+
+            for (long i = 2; i < n; i++)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        //зашифровать
+        private List<string> RSA_Encode(string s, long e, long n)
+        {
+            List<string> result = new List<string>();
+
+            BigInteger bi;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int index = Array.IndexOf(characters, s[i]);
+
+                bi = new BigInteger(index);
+                bi = BigInteger.Pow(bi, (int)e);
+
+                BigInteger n_ = new BigInteger((int)n);
+
+                bi = bi % n_;
+
+                result.Add(bi.ToString());
+            }
+
+            return result;
+        }
+
+        //расшифровать
+        private string RSA_Decode(List<string> input, long d, long n)
+        {
+            string result = "";
+
+            BigInteger bi;
+
+            foreach (string item in input)
+            {
+                bi = new BigInteger(Convert.ToDouble(item));
+                bi = BigInteger.Pow(bi, (int)d);
+
+                BigInteger n_ = new BigInteger((int)n);
+
+                bi = bi % n_;
+
+                int index = Convert.ToInt32(bi.ToString());
+
+                if (index >= 0 && index < characters.Length)
+                {
+                    result += characters[index].ToString();
+                }
+               
+            }
+
+            return result;
+        }
+
+        //вычисление параметра d. d должно быть взаимно простым с m
+        private long Calculate_d(long m)
+        {
+            long d = m - 1;
+
+            for (long i = 2; i <= m; i++)
+                if ((m % i == 0) && (d % i == 0)) //если имеют общие делители
+                {
+                    d--;
+                    i = 1;
+                }
+
+            return d;
+        }
+
+        //вычисление параметра e
+        private long Calculate_e(long d, long m)
+        {
+            long e = 10;
+
+            while (true)
+            {
+                if ((e * d) % m == 1)
+                    break;
+                else
+                    e++;
+            }
+
+            return e;
+        }
+
+
+
+
+
+
+
+
+    }
+}
